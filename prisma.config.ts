4deefa6265ff1cb94900933e50398625a93a70d0
Павел <pavelyasenko18@gmail.com>
2026-07-28 11:42:46 +0300

    add tg login

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "@prisma/config";
 
 export default defineConfig({
   datasource: {
-    url: "postgresql://pavel_yasenko@localhost:5432/ai_landing_db?schema=public",
+    url: "postgresql://neondb_owner:npg_4T8NhgQrBAPi@ep-solitary-dawn-auom8836.c-10.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
   },
 });
